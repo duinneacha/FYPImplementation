983fcc9f5eec2c5fecb4986dd0e9a441054c328a
Aidan Dennehy <duinneacha@gmail.com>
2021-04-08 08:47:46 +0100

    More work on add-recipe

--- src/components/modals/add-recipe.vue
+++ src/components/modals/add-recipe.vue
@@ -43,7 +43,7 @@
           </q-file>
 
          <q-input class="q-mb-xs" filled v-model="Recipe.title" label="Recipe Title" dense/>
-         <q-input class="q-mb-xs" filled v-model="Recipe.desctiption" label="Description" dense/>
+         <q-input class="q-mb-xs" filled v-model="Recipe.description" label="Description" dense/>
          <q-input class="q-mb-xs" filled v-model="Recipe.benefits" label="Benefits" dense/>
 
       </q-card-section>
@@ -63,14 +63,15 @@
             </q-toolbar>
 
             <div class="q-mb-xs" 
-               v-for="ingredient in Recipe.ingredients"
+               v-for="(ingredient, index) in Recipe.ingredients"
+               track-by="$index"
                :key="ingredient">
                         
                <q-banner rounded class="bg-grey-3">
                   <p class="text-h6">{{ ingredient }}</p>
                   <template v-slot:action>
-                    <q-btn color="negative" dense round icon="delete"  />
-                    <q-btn color="primary" dense round icon="edit"  />
+                    <q-btn color="negative" dense round icon="delete" @click.stop="deleteIngredient(index)" />
+                    <q-btn color="primary" dense round icon="edit" @click.stop="editIngredient(index)" />
                   </template>
                </q-banner>    
             </div>
@@ -85,14 +86,15 @@
            </q-toolbar>
 
            <div class="q-mb-xs" 
-               v-for="instruction in Recipe.instructions"
+               v-for="(instruction, index) in Recipe.instructions"
+               track-by="$index"
                :key="instruction">
                         
                <q-banner rounded class="bg-grey-3">
                   <p class="text-h6">{{ instruction }}</p>
                   <template v-slot:action>
-                    <q-btn color="negative" dense round icon="delete"  />
-                    <q-btn color="primary" dense round icon="edit"  />
+                    <q-btn color="negative" dense round icon="delete" @click.stop="deleteInstruction(index)" />
+                    <q-btn color="primary" dense round icon="edit" @click.stop="editInstruction(index)" />
                   </template>
                </q-banner>    
             </div> 
@@ -102,11 +104,14 @@
 
       </q-card-section>
 
-      <q-card-actions align="right">
-        <q-btn flat label="Save" color="primary" v-close-popup />
+      <!-- Save or Cancel the Add Recipe Operation -->
+      <q-card-actions align="center">
+        <q-btn class="q-mr-xl btn-fixed-width" label="Cancel" size="xl" color="warning" v-close-popup />
+        <q-btn class="q-ml-xl btn-fixed-width" label="Save" size="xl" color="positive" @click.stop="saveRecipe" />
+        
       </q-card-actions>
 
-
+    <!-- Add Instruction -->
     <q-dialog v-model="showAddInstruction"
           full-width 
           transition-show="slide-up"
@@ -141,7 +146,42 @@
           </q-card>
     </q-dialog>
 
+    <!-- Edit Instruction -->
+    <q-dialog v-model="showEditInstruction"
+          full-width 
+          transition-show="slide-up"
+          transition-hide="slide-down">
+          <q-card class="my-card">
 
+            <q-card-section class="row">
+             <q-toolbar class="bg-primary text-white">
+                 <q-toolbar-title>Edit Instruction</q-toolbar-title>
+                 <q-space />
+                 <q-btn 
+                   v-close-popup
+                   flat 
+                   rounded
+                   dense
+                   icon="close"
+                   />
+               </q-toolbar>
+            </q-card-section>
+
+
+            <q-card-section>
+              <q-input outlined v-model="line_instruction" label="Instruction" />
+
+            </q-card-section>
+            <q-card-section>
+              <div class="row">
+                 <q-btn class="q-ma-md" color="primary" label="Cancel" @click="showEditInstruction = false" />
+                 <q-btn class="q-ma-md" color="primary" label="Save" @click="saveEditInstruction"/>
+              </div>
+            </q-card-section>
+          </q-card>
+    </q-dialog>
+
+    <!-- Add Ingredient -->
     <q-dialog v-model="showAddIngredient"
        full-width 
        transition-show="slide-up"
@@ -166,6 +206,8 @@
            <q-input outlined v-model="line_ingredient" label="Ingredient" />
 
          </q-card-section>
+         
+         
          <q-card-section>
            <div class="row">
               <q-btn class="q-ma-md" color="primary" label="Cancel" @click="showAddIngredient = false" />
@@ -174,6 +216,42 @@
          </q-card-section>
        </q-card>
     </q-dialog>
+
+    <!-- Edit Ingredient -->
+    <q-dialog v-model="showEditIngredient"
+       full-width 
+       transition-show="slide-up"
+       transition-hide="slide-down">
+       <q-card class="my-card">
+         <q-card-section class="row">
+          <q-toolbar class="bg-primary text-white">
+              <q-toolbar-title>Edit Ingredient</q-toolbar-title>
+              <q-space />
+              <q-btn 
+                v-close-popup
+                flat 
+                rounded
+                dense
+                icon="close"
+                   />
+            </q-toolbar>
+         </q-card-section>
+
+
+         <q-card-section>
+           <q-input outlined v-model="line_ingredient" label="Ingredient" />
+
+         </q-card-section>
+         <q-card-section>
+           <div class="row">
+              <q-btn class="q-ma-md" color="primary" label="Cancel" @click="showEditIngredient = false" />
+              <q-btn class="q-ma-md" color="primary" label="Save" @click="saveEditIngredient"/>
+           </div>
+         </q-card-section>
+       </q-card>
+    </q-dialog>
+
+
   </q-card>  
 
   
@@ -186,29 +264,19 @@ export default {
       imageUpload: [],
       imageCaptured: false,
       showAddInstruction: false,
+      showEditInstruction: false,
       showAddIngredient: false,
+      showEditIngredient: false,
       line_instruction: '',
       line_ingredient: '',
+      editIndex: null,
       Recipe: {
         'title': '',
         'description': '',
         'benefits': '',
         'imageUrl': null,
-        'ingredients': [
-          'Ingredient 1',
-          'Ingredient 2',
-          'Ingredient 3',
-          'Ingredient 4',
-          'Ingredient 5',
-          'Ingredient 6',
-        ],
-        'instructions': [
-          'Instruction A',
-          'Instruction B',
-          'Instruction C',
-          'Instruction D',
-          'Instruction E',
-        ]
+        'ingredients': [],
+        'instructions': []
       }
     }
   },
@@ -240,7 +308,20 @@ export default {
       if (this.line_instruction) {
         this.Recipe.instructions.push(this.line_instruction)
         this.showAddInstruction = false
+        this.line_instruction = ''
       }
+    },
+    editInstruction(indx) {
+      console.log("In Edit Instruction . . .")
+      this.line_instruction = this.Recipe.instructions[indx]
+      this.editIndex = indx
+      this.showEditInstruction = true
+    },
+    saveEditInstruction(indx) {
+      console.log("In saveEditInstruction")
+      this.Recipe.instructions[this.editIndex] = this.line_instruction
+      this.showEditInstruction = false
+      this.line_instruction = ''
     },
     saveIngredient() {
       if (this.line_ingredient) {
@@ -248,7 +329,71 @@ export default {
         this.showAddIngredient = false
         this.line_ingredient = ''
       }
+    },
+    editIngredient(indx) {
+      console.log("In edit Ingredient . . .")
+      this.line_ingredient = this.Recipe.ingredients[indx]
+      this.showEditIngredient = true
+      this.editIndex = indx
+      console.log("index", indx)
+    },
+    saveEditIngredient() {
+      console.log("In saveEditIngredient")
+      this.Recipe.ingredients[this.editIndex] = this.line_ingredient
+      this.showEditIngredient = false
+      this.line_ingredient = ''
+    },
+    deleteInstruction(indx) {
+      console.log("In deleteInstruction . . .")
+            this.showDeleteIngredient = false
+       this.$q.dialog({
+        title: 'Confirm',
+        message: 'Would you like to Delete this Instruction?',
+        ok: {
+          push: true
+        },
+        cancel:{
+          color: 'negative'
+        },
+        persistent: false
+      }).onOk(() => {
+
+        // Delete Ingredient Item here
+        // console.log(this.Recipe)
+        let removeItem = this.Recipe.instructions.splice(indx, 1)
+
+      }).onCancel(() => {
+
+      })
+    },
+    deleteIngredient(indx) {
+      console.log("In deleteIngredient . . .", indx)
+      this.showDeleteIngredient = false
+       this.$q.dialog({
+        title: 'Confirm',
+        message: 'Would you like to Delete this Ingredient?',
+        ok: {
+          push: true
+        },
+        cancel:{
+          color: 'negative'
+        },
+        persistent: false
+      }).onOk(() => {
+
+        // Delete Ingredient Item here
+        // console.log(this.Recipe)
+        let removeItem = this.Recipe.ingredients.splice(indx, 1)
+
+      }).onCancel(() => {
+
+      })
+    },
+    saveRecipe() {
+      console.log("In save Recipe")
+      console.log("The Recipe is:", this.Recipe)
     }
+    
   },
   components: {
     // 'recipe-instructions': require('components/recipes/recipe-instructions.vue').default
@@ -266,4 +411,7 @@ export default {
  .camera-frame
    border: 2px solid $grey-10
    border-radius: 10px
+
+.btn-fixed-width
+  width: 200px
 </style>
